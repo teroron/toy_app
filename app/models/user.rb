1ec15c3b179225a97acfc9093278e9921f3632a1
teroron <a1908rk@aiit.ac.jp>
2020-05-14 12:25:30 +0000

    Finish toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :microposts
-    validates FILL_IN, presence: true    # 「FILL_IN」をコードに置き換えてください
-    validates FILL_IN, presence: true    # 「FILL_IN」をコードに置き換えてください
+    validates Name,length: { maximum: 20 }, presence: true    # 「FILL_IN」をコードに置き換えてください
+    validates Email,length: { maximum: 30 }, presence: true    # 「FILL_IN」をコードに置き換えてください
 end
